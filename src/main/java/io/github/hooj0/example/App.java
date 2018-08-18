212fe91cbb7755bae9fcfe5f1d499944e2898096
hoojo <hoojo@qq.com>
2018-08-18 15:48:53 +0800

    :sparkles: :bento: :tada:  Init add jib docker images application main

---
+++ src/main/java/io/github/hooj0/example/App.java
@@ -0,0 +1,71 @@
+package io.github.hooj0.example;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * jib docker images application main
+ * 
+ * @author hoojo
+ * @createDate 2018年8月18日 下午3:14:50
+ * @file App.java
+ * @package io.github.hooj0.example
+ * @project jib-docker-images
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	private final static Logger log = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		log.info("args: {}", new Object[] { args });
+
+		for (String arg : args) {
+			System.out.println("arg: " + arg);
+		}
+
+		httpPost();
+	}
+
+	private static void httpPost() {
+		String requestURL = System.getenv("REQUEST_URL");
+		log.info("request url: {}", requestURL);
+
+		try {
+			URI uri = URI.create(requestURL);
+			
+			HttpPost post = new HttpPost(uri);
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			
+			StringEntity entity = new StringEntity("http post time: " + System.currentTimeMillis(),
+					Charset.forName("UTF-8"));
+			entity.setContentEncoding("UTF-8");
+			post.setEntity(entity);
+			
+			HttpResponse response = httpClient.execute(post);
+			
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode != HttpStatus.SC_OK) {
+				log.error("request failure, status: {}", statusCode);
+			} else {
+				for (Header header : response.getAllHeaders()) {
+					log.debug("header {} -> {}", header.getName(), header.getValue());
+				}
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+}
